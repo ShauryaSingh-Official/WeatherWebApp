ac053abf93330115f6b1ea191811b4bd04496228
Shaurya Singh <souravk48560@gmail.com>
2023-12-27 23:03:20 +0530

    JS Logic Modified and Cleaned

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-let data;
-let tempeC;
-let locObj; // store user geolocation (fetched automatically)
-let cityNameProvided = document.getElementById("locationSearched");
+var unit = "metric";
 let getunit = document.getElementById("unit");
+let unitsymbol = document.getElementsByClassName("unitsymbol");
+
+let getWeatherBtn = document.getElementById("getWeather");
+let inputProvided = document.getElementById("locationSearched");
+
 let cityName = document.getElementById("city");
 let weather = document.getElementById("weatherType");
 let temperature = document.getElementById("temperature");
-let unitsym = document.getElementsByClassName("unitsym");
-
 let humidity = document.getElementById("humidity");
 let pressure = document.getElementById("pressure");
 let wind = document.getElementById("wind");
@@ -18,43 +18,82 @@ let longititude = document.getElementById("longititude");
 let feelsLike = document.getElementById("feelsLike");
 let country = document.getElementById("country");
 
-
-// #################################################get city###############################################################
-const getCity= async ()=>{
-  let URLZIPCode = "https://api.openweathermap.org/data/2.5/weather?zip="+cityNameProvided.value+",IN&appid=657cdd6c56a859874d12b8cae9f10ecf";
-  let zipdata = await fetch(URLZIPCode);
-  console.log(zipdata.json());
-}
-//   ####################################################################################################################
-
-var resp;
-const getWeather = async () => {
-  console.log(typeof cityNameProvided.value);
-  if(cityNameProvided.value ==Number){
-    getCity();
+// #################################################### Implement Unit Selection  ####################################################
+getunit.addEventListener('change',(e)=>{
+  if (e.target.value !== "metric"){ 
+    unit="imperial"  ;
+    unitsymbol[0].innerText = "Fah";
+    unitsymbol[1].innerText = "Fah";
   }
-  let url;
-    if(getunit.value==0){
-      url = "https://api.openweathermap.org/data/2.5/weather?q="+cityNameProvided.value+"&appid=657cdd6c56a859874d12b8cae9f10ecf&units=metric";
-    }
-    if(getunit.value==1){
-      url = "https://api.openweathermap.org/data/2.5/weather?q="+cityNameProvided.value+"&appid=657cdd6c56a859874d12b8cae9f10ecf&units=imperial";
-    }
-     
-      resp = await fetch(url);
+  else {
+    unit="metric";
+    unitsymbol[0].innerText = "°C"
+    unitsymbol[1].innerText = "°C"
+  }
+  // Call the API to fetch new/modified data 
+  updateWeatherData()
+})
+
+
+
+// ####################################################    GENERATE LINK For API   ####################################################
+
+function getURL(){
+  let base = "https://api.openweathermap.org/data/2.5/weather?";
+  let zIPOrCityName;
+  let endPath;
+
+  // https://api.openweathermap.org/data/2.5/weather?zip={zip code},{country code}&appid={API key}
+  // https://api.openweathermap.org/data/2.5/weather?zip=202001,IN&appid=657cdd6c56a859874d12b8cae9f10ecf&units=metric
+  if(+inputProvided.value >=1 ){
+    zIPOrCityName = "zip=";
+    endPath = ",in&appid=657cdd6c56a859874d12b8cae9f10ecf&units="+`${unit}`;
+    console.log(base+zIPOrCityName+inputProvided.value+endPath);
+  }
+  else {
+    zIPOrCityName= "q=";
+    endPath = "&appid=657cdd6c56a859874d12b8cae9f10ecf&units="+`${unit}`;
+  }
+  return ( base+zIPOrCityName+inputProvided.value+endPath) ;
+}
+
+// ####################################################  GET WEATHER  ####################################################
+
+const updateWeatherData = async ()=>{
+  let URL;
+  let resp;
+
+  // GENERATE URL
+  if(inputProvided.value){   URL = getURL(); }
+  else {alert("Invalid Data Provided !!!\n Please provide valid City name or ZIP Code. ")
+return;}
   
-      if (!resp.ok) {
-          alert(resp.status);
-          throw new Error("Error: ${resp.status}");
-        } else {
-          data = await resp.json();
-          setValues(data);
-    }
-  };
+  // MAKE ASYNCHRONOUS API REQUEST
+  try{
+    if(inputProvided){resp = await fetch(URL);}
   
-  const setValues = (data) => {
-    temperature.innerHTML = data.main.temp; // degree celcius / fahrenhiet
-    tempC = data.main.temp;
+    //  GET/EXTRACT DATA FROM RESPONSE
+    if (!resp.ok) {
+      throw new Error("Error: ${resp.status}");
+    } else {
+      data = await resp.json();
+      setValues(data);
+}
+  }
+  catch(error){
+    console.log("LOCATION NOT FOUND !!! \n Please provide valid an indian ZIPCode. OR \n Check your internet connection.");
+    alert("LOCATION NOT FOUND !!! \n Please provide valid an indian ZIPCode. OR \n Check your internet connection.");
+  }
+  
+}
+
+getWeatherBtn.addEventListener("click", updateWeatherData)
+
+
+// SET VALUES IN FRONTEND
+
+  function setValues(data){
+    temperature.innerHTML = data.main.temp; 
     humidity.innerHTML = data.main.humidity;
     pressure.innerHTML = data.main.pressure;
     wind.innerHTML = data.wind.speed; // km/hr
@@ -67,81 +106,3 @@ const getWeather = async () => {
     weather.innerHTML = data.weather[0].main;
     cityName.innerHTML = data.name;
   };
-
-  // ##################################################################################################################
-
-// #######################################################################################################################
-function getLocation() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showPosition);
-  } else {
-    alert("Geolocation is not supported by your browser.");
-  }
-}
-
-function showPosition(position) {
-  locObj = {
-    "latitude": (position.coords.latitude).toString(),
-    "longitude": (position.coords.longitude).toString()
-  };
-  console.log(locObj);
-  console.log(locObj.latitude+" , "+locObj.longitude)
-  let url2 = "https://api.openweathermap.org/data/2.5/weather?lat="+locObj.latitude+"&lon="+locObj.longitude+"&appid=657cdd6c56a859874d12b8cae9f10ecf&units=metric";
-    (async () => {
-         resp = await fetch(url2);    
-         if (!resp.ok) {
-             alert("${resp.status}");
-             throw new Error("Error: ${resp.status}");
-           } else {
-             data = await resp.json();
-             setValues(data);
-       }
-     })();
-}
-getLocation();
-
-
-// console.log();
-// ####################################################################Event listener
-const getWeatherBtn = document.getElementById("getWeather");
-getWeatherBtn.addEventListener("click", (e) => {
-  e.preventDefault();
-  if (!cityNameProvided) {
-    alert("Please provide city or zip code");
-  } else {
-    getWeather();
-  }
-});
-
-
-  let tempe ;
-  let tempflF;
-  
-  if(data){
-    tempe =data.main.temp ;
-    tempflF=data.main.feels_like;
-    console.log("${tempe}, ${tempflF}")  
-  }
-getunit.addEventListener("change",()=>{
-  console.log(getunit.value)
-    if(getunit.value==1){
-      console.log("Im reachable F")
-        tempe = (data.main.temp*9/5)+32;
-        tempflF = (data.main.feels_like*9/5)+32;
-        temperature.innerHTML = tempe.toFixed(2);
-        feelsLike.innerHTML = tempflF;
-        unitsym[0].innerHTML = "Fah";    
-        unitsym[1].innerHTML = "Fah";    
-    }
-    else if(getunit.value==0){
-      console.log("Im reachable")
-        temperature.innerHTML = ((tempe-32) * 5/9).toFixed(2) ;
-        feelsLike.innerHTML=(tempflF-32) * 5/9;
-        unitsym[0].innerHTML = "&deg;C";
-        unitsym[1].innerHTML = "&deg;C";
-    }
-})
-
-
-
-
